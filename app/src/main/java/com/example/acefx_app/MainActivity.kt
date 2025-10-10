6e4796415412a41729c4729db417228da7e830f8
bitturana-ab <b2ra830@gmail.com>
2025-10-10 13:28:01 +0530

    switching to signup screen on click but cant back

--- app/src/main/java/com/example/acefx_app/MainActivity.kt
+++ app/src/main/java/com/example/acefx_app/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.example.acefx_app
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var nextBtn : Button
+    private lateinit var backToHome: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        println("onCreate()")
         setContentView(R.layout.activity_main)
+
+//        navigate next signup page
+        nextBtn = findViewById<Button>(R.id.nextBtn)
+        nextBtn.setOnClickListener(){
+            Toast.makeText(this,"Navigating to sign page now", Toast.LENGTH_SHORT).show()
+            setContentView(R.layout.home_activity)
+        }
+//        this wont work because can find id from diff xml file
+//        back  to home
+//        backToHome = findViewById<Button>(R.id.backToHomeBtn)
+//        backToHome.setOnClickListener() {
+//            Toast.makeText(this,"Navigating to home page now", Toast.LENGTH_SHORT).show()
+//            setContentView(R.layout.activity_main)
+//        }
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        println("onResume()")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("onStop()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy()")
+    }
+
+
 }
