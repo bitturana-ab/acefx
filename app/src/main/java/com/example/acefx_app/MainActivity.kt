0cc3479ff8487d0ff72ade436e2254519dd1813f
bitturana-ab <b2ra830@gmail.com>
2025-10-10 17:23:48 +0530

    access  emailtext and toast show for test

--- app/src/main/java/com/example/acefx_app/MainActivity.kt
+++ app/src/main/java/com/example/acefx_app/MainActivity.kt
@@ -2,17 +2,31 @@ package com.example.acefx_app
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     private lateinit var nextBtn : Button
+    private lateinit var submitBtn : Button
+
     private lateinit var backToHome: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         println("onCreate()")
         setContentView(R.layout.activity_main)
+
+//        submit btn handle
+        submitBtn = findViewById<Button>(R.id.submitBtn)
+        submitBtn.setOnClickListener {
+            // access email for db call later
+            var textEmail = findViewById<EditText>(R.id.emailInput).text.toString()
+
+            Toast.makeText(this,"OTP sent to your $textEmail email!", Toast.LENGTH_SHORT).show()
+
+        }
 
 //        navigate next signup page
         nextBtn = findViewById<Button>(R.id.nextBtn)
