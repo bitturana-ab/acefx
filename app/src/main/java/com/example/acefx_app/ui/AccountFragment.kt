2558cd5fc62ce7ef8c9728c4e1c1b60c081a2b52
bitturana-ab <b2ra830@gmail.com>
2025-10-30 10:15:35 +0530

    logout text color set to red

--- app/src/main/java/com/example/acefx_app/ui/AccountFragment.kt
+++ app/src/main/java/com/example/acefx_app/ui/AccountFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -81,7 +82,7 @@ class AccountFragment : Fragment() {
 
     // logout function with clear token and navigate to login screen
     private fun logoutUser() {
-        AlertDialog.Builder(requireContext())
+        val dialog = AlertDialog.Builder(requireContext())
             .setTitle("Logout Confirmation")
             .setMessage("Are you sure you want to logout?")
             .setPositiveButton("Logout") { _, _ ->
@@ -122,6 +123,13 @@ class AccountFragment : Fragment() {
             }
             .setNegativeButton("Cancel", null)
             .show()
+        // Make Logout button text red
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+            .setTextColor(ContextCompat.getColor(requireContext(), android.R.color.holo_red_light))
+
+        // Optional: Make cancel button gray
+        dialog.getButton(AlertDialog.BUTTON_NEGATIVE)
+            .setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
     }
 
     //     Load user details instantly from SharedPreferences
