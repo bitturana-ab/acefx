67d8b47062cef58afe78ee954a46e217f1f0c1b6
bitturana-ab <b2ra830@gmail.com>
2025-10-30 10:21:54 +0530

    update profile confirmation dialog added

--- app/src/main/java/com/example/acefx_app/ui/ClientProfileFragment.kt
+++ app/src/main/java/com/example/acefx_app/ui/ClientProfileFragment.kt
@@ -1,11 +1,13 @@
 package com.example.acefx_app.ui
 
+import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -42,7 +44,7 @@ class ClientProfileFragment : Fragment() {
         val sharedPref = requireContext().getSharedPreferences("UserSession", Context.MODE_PRIVATE)
         token = sharedPref.getString("authToken", "") ?: ""
 
-        binding.nextBtn.setOnClickListener {
+        binding.updateProfileBtn.setOnClickListener {
             val clientNameInput = binding.clientNameInput.text.toString().trim()
             val companyName = binding.companyNameInput.text.toString().trim()
             val phoneNumber = binding.phoneInput.text.toString().trim()
@@ -60,7 +62,27 @@ class ClientProfileFragment : Fragment() {
                 }
 
                 else -> {
-                    saveClientProfile(clientNameInput, companyName, phoneNumber, pinCode)
+                    val dialog = AlertDialog.Builder(requireContext())
+                        .setTitle("Update Profile Confirmation")
+                        .setMessage("Are you sure you want to update your profile?")
+                        .setPositiveButton("Update Profile") { _, _ ->
+                            saveClientProfile(clientNameInput, companyName, phoneNumber, pinCode)
+                        }.setNegativeButton("Cancel", null)
+                        .create()
+
+                    dialog.show()
+                    // Make Logout button text red
+                    dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+                        .setTextColor(
+                            ContextCompat.getColor(
+                                requireContext(),
+                                android.R.color.holo_red_light
+                            )
+                        )
+
+                    // Optional: Make cancel button gray
+                    dialog.getButton(AlertDialog.BUTTON_NEGATIVE)
+                        .setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
                 }
             }
         }
@@ -93,7 +115,7 @@ class ClientProfileFragment : Fragment() {
                             Context.MODE_PRIVATE
                         )
                         sharedPref.edit().apply {
-                            putString("name",clientNameInput)
+                            putString("name", clientNameInput)
                             putString("companyName", companyName)
                             putString("phoneNumber", phoneNumber)
                             putString("pinCode", pinCode)
